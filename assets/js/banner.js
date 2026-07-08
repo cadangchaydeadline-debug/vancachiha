2df939c8ba812ad13cc36be0a0e35555625ba5a6
Vân Ca Chi Hạ <cadangchaydeadline@gmail.com>
2026-07-08 14:50:04 +0700

    Update banner.js

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,1 +1,40 @@
-// Banner
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let current = 0;
+
+function showSlide(i){
+
+    slides.forEach(s=>s.classList.remove("active"));
+    dots.forEach(d=>d.classList.remove("active"));
+
+    slides[i].classList.add("active");
+    dots[i].classList.add("active");
+
+    current=i;
+
+}
+
+dots.forEach((dot,index)=>{
+
+    dot.onclick=function(){
+
+        showSlide(index);
+
+    }
+
+});
+
+setInterval(function(){
+
+    current++;
+
+    if(current>=slides.length){
+
+        current=0;
+
+    }
+
+    showSlide(current);
+
+},5000);
